8f3550a8268b3782092fa62d1ddb96593b46a485
Ketan1004 <ashishshrikhande250@gmial.com>
2024-03-10 21:51:49 +0530

    New project Added

---
+++ 01basics-react/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Chai Aur React | Ashish</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function App() {
+  
+
+  return (
+   <h1> Chai Aur React | Ashish </h1>
+  )
+}
+
+export default App
