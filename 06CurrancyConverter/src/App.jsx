3c601b01e7b73b45269b71deacc0c620456407ef
Ketan1004 <ashishshrikhande250@gmail.com>
2024-03-18 14:18:35 +0530

    changes done

--- 06CurrancyConverter/src/App.jsx
+++ 06CurrancyConverter/src/App.jsx
@@ -4,11 +4,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-   <div></div>
+   <h1 className='text-3xl bg-orange-300'></h1>
   )
 }
 
 export default App
+ 
